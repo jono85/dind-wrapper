dfa7d39f7bf194d5d19ff9229ae93a9ba5f2ec4b
Josef Novak <josefnovak85@gmail.com>
2020-02-26 16:29:06 +0000

    added docker run hello-world to starter script to test and illustrate DIND working properly

--- data/scripts/00-start.sh
+++ data/scripts/00-start.sh
@@ -7,5 +7,11 @@ echo "{$SCRIPTNUM} Starter script launched."
 echo "{$SCRIPTNUM} Sleeping for 5 seconds."
 sleep 5
 
+echo "{$SCRIPTNUM} Running 'hello-world' container to test DIND is working properly..."
+
+echo "{$SCRIPTNUM} \$\$\$ docker run --rm hello-world"
+docker run --rm hello-world
+
+echo "{$SCRIPTNUM} "
 echo "{$SCRIPTNUM} Starter script finished, exiting."
 echo "{$SCRIPTNUM} "
